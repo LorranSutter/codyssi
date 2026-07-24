ff9e92f08e0ba4f4259227741a483bccc4a074d7
LorranSutter <sutterlorran@gmail.com>
2026-07-24 11:34:50 -0400

    2024 problem 1 documentation

--- 2024/problem01/main.py
+++ 2024/problem01/main.py
@@ -5,17 +5,23 @@ from utils.args import parse_args
 from utils.timer import timer
 
 """
-Preprocessing:
--
-
 Part 1:
--
+- Nothing fancy here, we just sum up all 300 prices straight from the file.
 
 Part 2:
--
+- The trick is sorting the prices in ascending order, so the priciest items end up at the tail of the list.
+  Slicing off the last 20 elements removes the 20 most expensive items in one shot, and we sum whatever's left.
 
 Part 3:
--
+- The key insight is that "add, subtract, add, subtract, ..." is just adding every price at an even index
+  (1st, 3rd, 5th, ...) and subtracting every price at an odd index (2nd, 4th, 6th, ...). Python slicing does this
+  cleanly: `prices[::2]` grabs every other price starting from the first, `prices[1::2]` grabs every other price
+  starting from the second, and negating the second group before summing both gives the final price.
+
+  Using the puzzle's own 6-item example (912372, 283723, 294281, 592382, 721395, 91238):
+    prices[::2]  = [912372, 294281, 721395]   (1st, 3rd, 5th)
+    prices[1::2] = [283723, 592382, 91238]    (2nd, 4th, 6th), negated
+    sum: 912372 + 294281 + 721395 - 283723 - 592382 - 91238 = 960705
 """
 
 
